af3768b845c1fe674f5c2ebfa3c08af2544d2cbe
Patryk Marks <patryk.marks@asseco.pl>
2021-03-30 10:28:30 +0200

    main class update (scanner and graph usage)

--- src/main/java/pl/patryk/recruitment/Task3.java
+++ src/main/java/pl/patryk/recruitment/Task3.java
@@ -3,6 +3,10 @@ package pl.patryk.recruitment;
 public class Task3 {
     public static void main(String[] args) {
 
+        CustomScanner customScanner = new CustomScanner();
+        int[][] scan = customScanner.scan();
+        Graph graph = new Graph();
+        graph.numberOfseparatedGraphs(scan);
     }
 
 }
